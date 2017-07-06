406e38b0f999b2cd901315d09ae40c8518489e1c
panxy <1851616111@qq.om>
2017-07-06 21:40:36 +0800

    吕鹏任务

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+files_path=/home/stage
+tomcat_path=/home/stage/tomcat
+
+${tomcat_path}/bin/shutdown.sh
+
+rm -rf /home/stage/tomcat/webapps/home
+rm /home/stage/tomcat/webapps/home.war
+
+mv ${files_path}/home.war  ${tomcat_path}/webapps/
+
+${tomcat_path}/bin/startup.sh
+
+sleep 1
+
+mv ${files_path}/cos-26Dec2008.jar ${tomcat_path}/webapps/home/WEB-INF/lib
+
+
